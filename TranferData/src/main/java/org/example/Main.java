d61a7d75ca7e944c24770451c087ddafaeea14a2
ArnutDev <arnutbuadonpai@gmail.com>
2025-02-26 12:40:24 +0700

    Update main

--- TranferData/src/main/java/org/example/Main.java
+++ TranferData/src/main/java/org/example/Main.java
@@ -14,26 +14,24 @@ public class Main {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode rootNode = objectMapper.readTree(new File(filePath));
-
             JsonNode nodesArray = rootNode.get("nodes");
             JsonNode edgesArray = rootNode.get("edges");
-
             List<String> Nodes = new ArrayList<>();
             List<String> addressIn = new ArrayList<>();
             List<String> addressOut = new ArrayList<>();
-
             for(JsonNode node : nodesArray){
                 String value = node.get("type").asText();
                 Nodes.add(value);
             }
-            int index = 0;//นับindex ของ addressIn แรกใส่ ''
+            boolean firstPosition = true;
             for(JsonNode edge : edgesArray){
                 String source = edge.get("source").asText();
-                if(index!=0){
+                if(!firstPosition){
                     addressIn.add(source);
                 }
                 else{
                     addressIn.add("\'\'");
+                    firstPosition = false;
                 }
                 boolean found = false;
                 String target = edge.get("target").asText();
@@ -41,21 +39,18 @@ public class Main {
                     String findSource = test.get("source").asText();
                     if(findSource.equals(target)){
                         addressOut.add(target);
-//                        System.out.println("source: "+nextSource+" target: "+target);
                         found = true;
                         break;
                     }
                 }
-                if(found==false && index!=0){
+                if(!found && !firstPosition){
                     addressOut.add("\'\'");
                 }
-                index++;
             }
 
             System.out.println("Nodes: " + Nodes);
             System.out.println("addressIn: " + addressIn);
             System.out.println("addressOut: " + addressOut);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
